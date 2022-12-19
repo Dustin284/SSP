e681a637bcde49a0f2694ad11bff663606976201
dustin-joeleden <dustin.joel.eden@gmail.com>
2022-12-19 12:26:31 +0100

    +

--- src/Game/SpielSSP.java
+++ src/Game/SpielSSP.java
@@ -23,8 +23,7 @@ public class SpielSSP {
 
         do {
             //Anweisung
-            //for debug:
-            System.out.println("Punkte PC: " + punktePC + " Punkte Spieler: " + punkteSpieler);
+            //for debug System.out.println("Punkte PC: " + punktePC + " Punkte Spieler: " + punkteSpieler);
             System.out.println(" ");
             System.out.println("Was nimmst du? Papier (P), Stein (S) oder Schere (X)?");
             String eingabe = scanner.nextLine().toUpperCase();
